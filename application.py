59ffd469855a305eb8cf40ae6dd4a391ad30d3f2
Gabrisamma <gabrisamma@gmail.com>
2021-12-24 02:36:22 +0100

    Added damage calculation string with percentages of damage done

--- application.py
+++ application.py
@@ -1,12 +1,11 @@
 import tkinter as tk
-from tkinter import StringVar, ttk, font
-from damage_calculation import damage_calc, get_move
+from tkinter import IntVar, ttk, font
+from damage_calculation import damage_calc, damage_perc, get_move, string_calc
 from pkm_stats import *
 
 class Application(tk.Tk):
     def __init__(self):
         
-        self.evs = []
         self.stat_entries = []
         self.stat_label = []
 
@@ -16,39 +15,53 @@ class Application(tk.Tk):
         self.defaultFont = font.nametofont("TkDefaultFont")
         self.defaultFont.configure(size=12)
 
-        self.pokemon = {"new_stats": {'hp':StringVar(), 'attack':StringVar(), 'defense':StringVar(), 'special-attack':StringVar(), 'special-defense':StringVar(), 'speed':StringVar()}}
+        validate_level_wrapper = self.register(self.validate_level)
+        validate_evs_wrapper = self.register(self.validate_evs)
+
+        self.pokemon = {"new_stats": {'hp':IntVar(), 'attack':IntVar(), 'defense':IntVar(), 'special-attack':IntVar(), 'special-defense':IntVar(), 'speed':IntVar()}}
 
         self.main_frame = tk.Frame(self)
         self.main_frame.grid()
 
-        self.pokemon_species = ttk.Combobox(self.main_frame, values=get_all_pokemon(), font=self.defaultFont)
+        self.pokemon['name'] = tk.StringVar()
+
+        self.pokemon_species = ttk.Combobox(self.main_frame, values=get_all_pokemon(), textvariable=self.pokemon['name'], font=self.defaultFont)
         self.pokemon_species.current(0)
         
         self.pokemon_species.bind('<<ComboboxSelected>>', self.set_base_stats)
         self.pokemon_species.bind('<<ComboboxSelected>>', lambda event: [self.set_final_stats(event, i) for i in range(6)], add='+')
         self.pokemon_species.grid()
 
-        self.level = StringVar(value=50)
-        self.level_spinbox = tk.Spinbox(self.main_frame, from_=0.0, to_=100.0, textvariable=self.level, state="readonly", font=self.defaultFont, width=5)
+        self.level = IntVar(value=50)
+        self.level_spinbox = tk.Spinbox(self.main_frame, from_=0.0, to_=100.0, textvariable=self.level, 
+                    validatecommand=(validate_level_wrapper, '%P'), validate='key',
+                    font=self.defaultFont,width=5)
+
         self.level_spinbox.bind('<ButtonRelease-1>', lambda event: [self.set_final_stats(event, i) for i in range(6)])
+        self.level_spinbox.bind('Key', lambda event: [self.set_final_stats(event, i) for i in range(6)])
         self.level_spinbox.grid()
 
         self.evs_frame = tk.Frame(self)
         self.evs_frame.grid(column=0)
 
-        self.nature_combobox = ttk.Combobox(self.main_frame, values=get_all_natures(), font=self.defaultFont)
+        self.pokemon['nature'] = tk.StringVar()
+        self.nature_combobox = ttk.Combobox(self.main_frame, values=get_all_natures(), textvariable=self.pokemon['nature'], font=self.defaultFont)
         self.nature_combobox.current(0)
         self.nature_combobox.bind('<<ComboboxSelected>>', lambda event: [self.set_final_stats(event, i) for i in range(6)])
         self.nature_combobox.grid()
 
         self.set_base_stats()
 
+        self.pokemon['evs'] = []
+        
         for i in range(6):
-            stat = tk.StringVar()
-            self.stat_entries.append(tk.Spinbox(self.evs_frame, from_=0.0, to_=252.0, textvariable=stat, increment=4, state="readonly", font=self.defaultFont, width=4))
+            stat = IntVar()
+            self.stat_entries.append(tk.Spinbox(self.evs_frame, from_=0.0, to_=252.0, 
+                        textvariable=stat, validatecommand=(validate_evs_wrapper, '%P'), validate="key",
+                        increment=4, font=self.defaultFont, width=4))
             self.stat_entries[i].bind('<ButtonRelease-1>', lambda event, i=i:self.set_final_stats(event, i))
             self.stat_entries[i].grid(column=0, row=i)
-            self.evs.append(stat)
+            self.pokemon['evs'].append(stat)
 
             self.set_final_stats(None, i)
 
@@ -61,23 +74,27 @@ class Application(tk.Tk):
 
 
     def set_base_stats(self, event=None):
-        self.pokemon.update(get_pokemon_stats(self.pokemon_species.get()))
+        self.pokemon.update(get_pokemon_stats(self.pokemon['name'].get()))
 
 
     def set_final_stats(self, event, i):
-        self.pokemon['new_stats'][self.index_to_stat(i)].set(calculate_stat(self.pokemon['stats'][i]['stat']['name'], self.pokemon['stats'][i]['base_stat'], int(self.level.get()), int(self.evs[i].get()), nature_calc(self.nature_combobox.get(), self.pokemon['stats'][i]['stat']['name'])))
+        self.pokemon['new_stats'][self.index_to_stat(i)].set(calculate_stat(self.pokemon['stats'][i]['stat']['name'], self.pokemon['stats'][i]['base_stat'], self.level.get(), self.pokemon['evs'][i].get(), nature_calc(self.nature_combobox.get(), self.pokemon['stats'][i]['stat']['name'])))
 
     def calc_all(self):
         for mon in get_most_used():
             stats = get_pokemon_stats(mon)
             spreads = get_pokemon_spreads(mon)
             for i in range(len(spreads['evs'])):
+                evs = split_evs(spreads['evs'][i])
                 new_stats = {}
                 for j, stat in enumerate(stats['stats']):
-                    new_stats[self.index_to_stat(j)] = calculate_stat(stat['stat']['name'], stat['base_stat'], 50, int(split_evs(spreads['evs'][i])[j]), nature_calc(spreads['nature'][i].lower(), stat['stat']['name']))
-                new_pokemon = { 'new_stats': new_stats,
+                    new_stats[self.index_to_stat(j)] = calculate_stat(stat['stat']['name'], stat['base_stat'], 50, int(evs[j]), nature_calc(spreads['nature'][i].lower(), stat['stat']['name']))
+                new_pokemon = { 'name': mon,
+                                'evs': evs,
+                                'new_stats': new_stats,
                                 'type': stats['type']}
-                print(damage_calc(int(self.level.get()), self.pokemon, new_pokemon, get_move('psyshock'), 1, False, False), flush=True)
+                min, max = damage_calc(int(self.level.get()), self.pokemon, new_pokemon, get_move('psyshock'), 1, False, False)
+                print(string_calc(self.pokemon, new_pokemon, get_move('psyshock'), min, max), flush=True)
 
 
     def index_to_stat(self, i):
@@ -88,3 +105,24 @@ class Application(tk.Tk):
                     4: 'special-defense',
                     5: 'speed'}
         return options[i]
+
+
+    def validate_level(self, level):
+        try:
+            level = int(level)
+            if level > 0 and level <= 100:
+                #for i in range(6): self.set_final_stats(None, i)
+                return True
+        except Exception as e:
+            print("level", e, flush=True)
+        return False
+
+    
+    def validate_evs(self, evs):
+        try:
+            evs = int(evs)
+            if evs > 0 and evs <= 252:
+                return True
+        except Exception as e:
+            print("evs", e, flush=True)
+        return False
--- damage_calculation.py
+++ damage_calculation.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import math
-from pkm_stats import pokeapi_url
+from pkm_stats import nature_calc, pokeapi_url
 
 
 def get_all_moves():
@@ -18,6 +18,7 @@ def get_move(name):
     r = requests.get(pokeapi_url + 'move/' + name)
     if r.status_code:
         move = dict()
+        move['name'] = name
         move['damage_class'] = r.json()['damage_class']['name']
         move['power'] = r.json()['power']
         move['stat_changes'] = r.json()['stat_changes']
@@ -26,6 +27,9 @@ def get_move(name):
     else:
         print("couldn't get move information")
 
+
+def damage_class(dmg_class):
+    return '' if dmg_class == "physical" else 'special-'
 
 def damage_split_targets(targets):
     return 1 if targets == 1 else 0.75
@@ -48,7 +52,7 @@ def damage_calc(level, attacker, defender, move, targets, critical, burn):
     if move['power'] == 0:
         return 0, 0
 
-    attack = '' if move['damage_class'] else 'special-'
+    attack = damage_class(move['damage_class'])
 
     max_damage = ((2 * level / 5) * move['power'] * float(attacker['new_stats'][attack + 'attack'].get()) / defender['new_stats'][attack + 'defense'] / 50 + 2)
     max_damage *= damage_split_targets(targets) * is_critical(critical) * is_STAB(move['type'], attacker['type']) * type_effectiveness(move['type'], defender['type']) * is_burned(burn)
@@ -67,3 +71,43 @@ def type_effectiveness(move_type, pokemon_types):
         damage *= type_chart[move_type['name']][pokemon_type['type']['name']]
     f.close()
     return damage
+
+
+def damage_perc(dmg, hp):
+    return str(round(dmg / hp * 100, 1))
+
+
+def string_calc(attacker, defender, move, min, max):
+    attack = damage_class(move['damage_class'])
+    new_string = str(attacker['evs'][stat_to_index(attack + "attack")].get())
+
+    if (nature_calc(attacker['nature'].get(), attack + "attack")) == 1.1:
+        new_string += "+ "
+    elif (nature_calc(attacker['nature'].get(), attack + "attack")) == 0.9:
+        new_string += "- "
+
+    new_string += "Atk " if attack == '' else "SpA "
+
+    new_string += attacker['name'].get().capitalize() + " "
+    new_string += move['name'].capitalize() + " "
+
+    new_string += "vs. "
+
+    new_string += defender['evs'][stat_to_index('hp')] + " HP / "
+    new_string += defender['evs'][stat_to_index(attack + "defense")] + " "
+
+    new_string += "Def " if attack == '' else "SpD "
+    new_string += defender['name'].capitalize()
+
+    new_string += ": " + str(min) + "-" + str(max) + " (" + damage_perc(min, defender['new_stats']['hp']) + " - " + damage_perc(max, defender['new_stats']['hp']) + "%)"
+    return new_string
+
+
+def stat_to_index(stat):
+        options = { 'hp': 0,
+                    'attack': 1,
+                    'defense': 2,
+                    'special-attack': 3,
+                    'special-defense': 4,
+                    'speed': 5}
+        return options[stat]
